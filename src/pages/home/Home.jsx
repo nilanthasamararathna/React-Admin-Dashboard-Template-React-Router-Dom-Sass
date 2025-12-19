525cdccfe36b4dc3cac25e96fb6c52dd8a592702
E.Nilantha Samararathna <45939807+nilanthasamararathna@users.noreply.github.com>
2025-12-19 23:37:21 +0530

    create top navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import './home.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
 
 const Home = () => {
   return (
     <div className='home'>
         <Sidebar />
-        <div className="homeContainer">Container</div>
+        <div className="homeContainer">
+          <Navbar />
+        </div>
     </div>
   )
 }
